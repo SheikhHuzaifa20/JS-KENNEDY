4c6031bf5062857471e43bf5040aa6cc0e7c6841
Irtiza Ahmed <54776755+IrtizaAhmed131198@users.noreply.github.com>
2025-10-10 04:31:54 +0500

    Update 2025_10_09_203040_create_blog_reviews_table.php

--- database/migrations/2025_10_09_203040_create_blog_reviews_table.php
+++ database/migrations/2025_10_09_203040_create_blog_reviews_table.php
@@ -15,14 +15,12 @@ return new class extends Migration
     {
         Schema::create('blog_reviews', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('blog_id');
+            $table->integer('blog_id');
             $table->string('name');
             $table->string('email');
             $table->text('message');
             $table->tinyInteger('rating')->default(5);
             $table->timestamps();
-
-            $table->foreign('blog_id')->references('id')->on('blogs')->onDelete('cascade');
         });
     }
 
