f8705eb249a18349dc9fc86c7a882bd9f5e4380c
Sheikh Huzaifa Farooqi <155949017+SheikhHuzaifa20@users.noreply.github.com>
2025-10-10 20:24:51 +0500

    limit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -113,7 +113,7 @@ class HomeController extends Controller
 
     public function blogdetail($id)
     {
-        $reviews = DB::table('blog_reviews')->where('blog_id', $id)->latest()->get();
+        $reviews = DB::table('blog_reviews')->where('blog_id', $id)->latest()->limit(4)->get();
         $blog = blog::findOrFail($id);
 
         return view('blog_detail', compact('blog', 'reviews'));
