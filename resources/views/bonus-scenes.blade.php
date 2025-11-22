d7b4a8fe04f2bce0be8cfd52a2d1abb66b57a335
Sheikh Huzaifa Farooqi <155949017+SheikhHuzaifa20@users.noreply.github.com>
2025-11-22 21:31:06 +0500

    done

--- resources/views/bonus-scenes.blade.php
+++ resources/views/bonus-scenes.blade.php
@@ -2,6 +2,7 @@
     $page = \Illuminate\Support\Facades\DB::table('pages')->where('id', 35)->first();
     $sections = \Illuminate\Support\Facades\DB::table('section')->where('page_id', 35)->get();
     $banners = \Illuminate\Support\Facades\DB::table('banners')->get();
+    $links = \Illuminate\Support\Facades\DB::table('products')->where('id', 13)->first();
 @endphp
 @extends('layouts.app')
 @section('title', 'Home')
@@ -34,19 +35,15 @@
                         <h2>{!! $sections[0]->value !!}</h2>
                         {!! $page->content !!}
                     </div>
-
-                    @php
-                        $link = \Illuminate\Support\Facades\DB::table('products')->where('id', 33)->get();
-                    @endphp
                     <div class="bonus-scene-version">
                         <div class="santa-version">
-                            <a href="#" class="btn snata-btn">Secret Santa <img
+                            <a href="{{$links->link}}" class="btn snata-btn">Secret Santa <img
                                     src="{{ asset('asset/images/btn-arrow.png') }}" class="img-fluid" alt=""></a>
                             <p>Book 3 – Curious as to who got whom in the
                                 Green family Secret Santa. Click to find out!</p>
                         </div>
                         <div class="santa-version">
-                            <a href="#" class="btn snata-btn">New Arrivals <img
+                            <a href="{{$links->link}}" class="btn snata-btn">New Arrivals <img
                                     src="{{ asset('asset/images/btn-arrow.png') }}" class="img-fluid" alt=""></a>
                             <p>Book 3 – Interested in the first meeting of
                                 Quinn, Alec, and the kids. Well, here it is.</p>
